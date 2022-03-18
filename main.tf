07ee69f8e504d09a866696c4a669a5cfe03aafae
arun-kumar34 <arun.kumar@bootlabstech.com>
2022-03-18 10:07:50 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "google_secret_manager_secret" "secret" {
+  secret_id = var.secret_id
+  labels    = length(keys(var.labels)) < 0 ? null : var.labels
+
+  dynamic "replication" {
+    for_each = var.replication_automatic != 0 ? [1] : []
+    content {
+      automatic = true
+    }
+  }
+
+  dynamic "replication" {
+    for_each = !var.replication_automatic && length(var.replication_user_managed_replicas) != 0 ? [1] : []
+    content {
+      user_managed {
+        dynamic "replicas" {
+          for_each = var.replication_user_managed_replicas
+          content {
+            location                    = replicas.value.location
+            customer_managed_encryption = replicas.value.customer_managed_encryption
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret-version" {
+  secret      = google_secret_manager_secret.secret.id
+  secret_data = var.secret_data
+}
---
+++ variables.tf
@@ -0,0 +1,36 @@
+//required variables
+variable "secret_id" {
+  type        = string
+  description = "This must be unique within the project."
+}
+
+variable "project" {
+  type        = string
+  description = "The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
+}
+
+variable "labels" {
+  type        = map(any)
+  description = "A set of key/value label pairs to assign to this ManagedZone."
+  default     = {}
+}
+
+variable "replication_automatic" {
+  type        = bool
+  description = "The Secret will automatically be replicated without any restrictions."
+  default     = true
+}
+
+variable "replication_user_managed_replicas" {
+  type = list(object({
+    location                    = string,
+    customer_managed_encryption = string
+  }))
+  description = "The list of Replicas for this Secret."
+  default     = []
+}
+
+variable "secret_data" {
+  type        = string
+  description = "The secret data. Must be no larger than 64KiB. Note: This property is sensitive and will not be displayed in the plan."
+}
