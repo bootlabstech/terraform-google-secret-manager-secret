4cd26fded29787e0929509c5bbb019d04812f3a6
arun-kumar34 <arun.kumar@bootlabstech.com>
2022-03-26 23:08:12 +0530

    initial commit

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 resource "google_secret_manager_secret_iam_binding" "binding" {
-  count     = var.secretAccessorMembers != null && length(var.secretAccessorMembers) != 0 ? length(var.secretAccessorMembers) : 0
+  count     = var.secretAccessorMembers != null && length(var.secretAccessorMembers) != 0 ? 1 : 0
   project   = google_secret_manager_secret.secret.project
   secret_id = google_secret_manager_secret.secret.secret_id
   role      = "roles/secretmanager.secretAccessor"
